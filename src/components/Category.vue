93ef232650b93bb226d079bb6e0cfa63ea111f12
goldeneyezhang <goldeneyezhang@163.com>
2017-09-07 19:37:52 +0800

    route

---
+++ src/components/Category.vue
@@ -0,0 +1,8 @@
+<template>
+<div>首页</div>
+</template>
+<style>
+</style>
+<script>
+export default{}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-
+// 引入创建的四个页面
+import Home from '@/components/Home'
+import Category from '@/components/Category'
+import Cart from '@/components/Cart'
+import Me from '@/components/Me'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +14,26 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/category',
+      name: 'Category',
+      component: Category
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/me',
+      name: 'Me',
+      component: Me
     }
   ]
 })
